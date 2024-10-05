f40ee542771a8c6d3874e12a47bcb6c97c44c91d
JinWu <gin01226@naver.com>
2024-10-05 17:35:43 +0900

    Feat: add mul, div function

--- calculater_ver1.py
+++ calculater_ver1.py
@@ -5,7 +5,15 @@ def cus_sum(a, b):
 #뺄셈함수
 def cus_sub(a, b):
     return a - b
-
+#곱셈함수
+def cus_mul(a, b):
+    return a * b
+#나눗셈함수
+def cus_div(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return None
 #예외처리
 def cus_except(a, b):
     return '올바르지 않은 입력입니다.'
@@ -13,6 +21,8 @@ def cus_except(a, b):
 switch_dict = {
     'sum': cus_sum,
     'sub': cus_sub,
+    'mul': cus_mul,
+    'div': cus_div
 }
 #입력된 연산자를 통해 맵핑된 함수 탐색
 def detect_case(value, x, y):
@@ -22,6 +32,8 @@ def detect_case(value, x, y):
 print("--------------- 입력 양식 ---------------")
 print("-- 덧셈: sum a b (and result => a + b)")
 print("-- 뺄셈: sub a b (and result => a - b)")
+print("-- 곱셈: mul a b (and result => a * b)")
+print("-- 나눗셈: div a b (and result => a / b)")
 print("-- 종료: exit (and result => shut down)")
 while True:
     # 종료조건 탐색
